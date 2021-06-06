9e91e506fd0936e87c989b09d3786ddbbcfc542b
Mayank718 <85431844+Mayank718@users.noreply.github.com>
2021-06-06 13:27:07 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        Myself, Mayank Kumar. I have attended the whole 7 days bootcamp on web
+        development and react.js. Thank You.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
